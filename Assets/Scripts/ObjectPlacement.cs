ace02164bd181d494f4102c38bf68ade90bd474f
AntonyScott <antonyscott1@hotmail.co.uk>
2023-01-09 03:09:40 +0000

    Commit 8

--- Assets/Scripts/ObjectPlacement.cs
+++ Assets/Scripts/ObjectPlacement.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.EventSystems;
 [RequireComponent(typeof(ARRaycastManager))]
-public class PlacementControllerWithMultiple : MonoBehaviour
+public class ObjectPlacement : MonoBehaviour
 {
     [SerializeField]
     private Button xbox360Btn;
@@ -31,11 +31,81 @@ public class PlacementControllerWithMultiple : MonoBehaviour
     }
     void Update()
     {
+        if (placedPrefab == null)
+        {
+            return;
+        }
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                var touchPosition = touch.position;
+                bool isOverUI = EventSystem.current.IsPointerOverGameObject(touch.fingerId);
+                Debug.Log(isOverUI);
+                if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+                {
+                    Debug.Log(" blocked raycast");
+                    return;
+                }
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hit;
 
+                if (Physics.Raycast(ray, out hit) && (hit.transform.tag == "knight"))
+                {
+                    Debug.Log(" raycast");
+                    if (Input.GetTouch(0).deltaTime > 0.2f)
+                    {
+                        Destroy(hit.transform.gameObject);
+                    }
+                }
+                else if (!isOverUI && arRaycastManager.Raycast(touchPosition, hits,
+               UnityEngine.XR.ARSubsystems.TrackableType.Planes))
+                {
+                    Debug.Log(" arraycast");
+                    var hitPose = hits[0].pose;
+                    Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+                }
+            }
+        }
     }
 
-    void ChangePrefabTo()
+        void ChangePrefabTo(string prefabName)
     {
         placedPrefab = Resources.Load<GameObject>($"prefabs/xbox/{prefabName}");
+
+        if (placedPrefab == null)
+        {
+            Debug.LogError($"Prefab with name {prefabName} could not be loaded, make sure you check the naming of your prefabs...");
+        }
+
+        Color colBtn1 = xbox360Btn.image.color;
+        Color colBtn2 = xboxOneBtn.image.color;
+        Color colBtn3 = xboxSeriesBtn.image.color;
+
+        switch (prefabName)
+        {
+            case "Xbox 360":
+                colBtn1.a = 1f;
+                colBtn2.a = 0.5f;
+                colBtn3.a = 0.5f;
+                break;
+
+            case "Xbox One":
+                colBtn1.a = 0.5f;
+                colBtn2.a = 1f;
+                colBtn3.a = 0.5f;
+                break;
+
+            case "Xbox Series":
+                colBtn1.a = 0.5f;
+                colBtn2.a = 0.5f;
+                colBtn3.a = 1f;
+                break;
+        }
+
+        xbox360Btn.image.color = colBtn1;
+        xboxOneBtn.image.color = colBtn2;
+        xboxSeriesBtn.image.color = colBtn3;
     }
 }
